b17925659c8de4c0886ed2a08da08b193d8c1364
Vladimir Dvornikov <echobizarre@gmail.com>
2022-05-16 19:43:28 +0300

    Initial Commit

---
+++ PearsonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PearsonsList
+//
+//  Created by Vladimir Dvornikov on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
